5a4f5db098a561a9a97992a82879b0ecb88775a2
VadymIevtushenko <boroforit@gmail.com>
2017-01-15 13:29:11 +0200

    1

--- test__3.py
+++ test__3.py
@@ -11,23 +11,20 @@ class Test3(unittest.TestCase):
     def setUp(self):
         self.driver = webdriver.Firefox()
         self.driver.implicitly_wait(30)
-        self.base_url = "https://portal.bugfinders.com/6d6002216bdce0ff17d66ea10534ff707a79dcf54cac759d8db85e7761e4e4a7979d19e240ae662811790fce089fb8549f2b6bc6f5674ec7816a1da6df27ab4emwrxnZhqcCClFF5RvXgivVNNOoEZmCSPPjat_2FHj_2BBEI_3D"
+        self.base_url = "https://www.google.ru/"
         self.verificationErrors = []
         self.accept_next_alert = True
     
     def test_3(self):
         driver = self.driver
-        driver.get(self.base_url + "/")
-        driver.find_element_by_id("un").clear()
-        driver.find_element_by_id("un").send_keys("boroforit@gmail.com")
-        driver.find_element_by_id("pw").clear()
-        driver.find_element_by_id("pw").send_keys("aF5JGsjsn5zEqqv")
-        driver.find_element_by_css_selector("input.button").click()
-        driver.find_element_by_link_text("My Devices").click()
-        driver.find_element_by_link_text("Windows 10").click()
-        driver.find_element_by_link_text("Account details").click()
-        driver.find_element_by_link_text("Functional projects").click()
-        driver.find_element_by_link_text("Log out").click()
+        driver.get(self.base_url + "about:home")
+        driver.find_element_by_id("searchText").clear()
+        driver.find_element_by_id("searchText").send_keys(u"фейсбук")
+        driver.find_element_by_id("searchSubmit").click()
+        driver.find_element_by_link_text(u"Увійти").click()
+        # ERROR: Caught exception [ERROR: Unsupported command [selectWindow | name=_e_0D3u | ]]
+        driver.find_element_by_id("searchText").clear()
+        driver.find_element_by_id("searchText").send_keys("")
     
     def is_element_present(self, how, what):
         try: self.driver.find_element(by=how, value=what)
